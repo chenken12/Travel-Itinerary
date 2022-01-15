6c125f94be6c6e077c38526824e41d60854ab26d
Kenneth Chen <kenchen0606@gmail.com>
2022-01-15 17:25:21 -0500

    fix marker css

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,34 @@ import Marker from './components/Marker';
 
 const App = () => {
   const [center, setCenter] = useState({lat: 43.6532, lng: -79.3832 });
-  const [zoom, setZoom] = useState(11);
+  const [zoom, setZoom] = useState(9);
 
   const {
       state,
       dispatch
   } = useApplicationData();
-    const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
-));
+  
+  const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>));
+
 return (
-  <div style={{ height: '100vh', width: '100%' }}>
+  <div style={{ height: '100vh', width: '70%', right: 0}}>
     <GoogleMapReact
       bootstrapURLKeys={{ key: process.env.REACT_APP_MAPKEY }}
       defaultCenter={center}
       defaultZoom={zoom}
     >
+
       <Marker
         lat={43.6532}
         lng={-79.3832}
         name="My Marker"
         color="blue"
+      />
+      <Marker
+        lat={43.5632}
+        lng={-79.7832}
+        name="My Marker Red"
+        color="red"
       />
     </GoogleMapReact>
   </div>

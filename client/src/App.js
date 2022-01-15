9ab1a406db9de7a57f9c77e119d7336232951e62
Kenneth Chen <kenchen0606@gmail.com>
2022-01-14 22:53:37 -0500

    add map and marker

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import useApplicationData from './hooks/useApplicationData';
+import GoogleMapReact from 'google-map-react';
+import Marker from './components/Marker';
 
 const App = () => {
+  const [center, setCenter] = useState({lat: 43.6532, lng: -79.3832 });
+  const [zoom, setZoom] = useState(11);
+
   const {
       state,
       dispatch
   } = useApplicationData();
     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
 ));
-return (<div className="App" >
-  <h1> Users </h1>
-
-  <ul> {userList} </ul>
-</div >
+return (
+  <div style={{ height: '100vh', width: '100%' }}>
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: process.env.REACT_APP_MAPKEY }}
+      defaultCenter={center}
+      defaultZoom={zoom}
+    >
+      <Marker
+        lat={43.6532}
+        lng={-79.3832}
+        name="My Marker"
+        color="blue"
+      />
+    </GoogleMapReact>
+  </div>
 );
 };
 

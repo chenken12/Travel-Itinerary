f3606f1a7896ae6ae284013d22917c784ef5b22e
Kenneth Chen <kenchen0606@gmail.com>
2022-01-16 18:11:24 -0500

    add routing buttons for test

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import './App.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useApplicationData from './hooks/useApplicationData';
 
 
 const App = () => {
+  const navigate = useNavigate();
   const {
       state,
       dispatch
   } = useApplicationData();
+
+  const handleRouteClick = (path = '/') => {
+    navigate(path);
+  }
   
   const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>));
 
@@ -18,6 +23,8 @@ return (
     <ul className="test"> {userList} </ul>
     {/* <Link to="/login">Invoices</Link> |{" "}
     <Link to="/register">Expenses</Link> */}
+    <button onClick={() => handleRouteClick("/addPins")} type="button">Add Pins page</button>
+    <button onClick={() => handleRouteClick("/usersTravels")} type="button">usersTravels page</button>
   </div>
 );
 };

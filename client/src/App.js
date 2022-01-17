d688cdd0379e88602af18ad9f594fc275e731d70
amnahasad <amnah_asad@hotmail.com>
2022-01-17 16:09:18 -0500

    Add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { Link } from "react-router-dom";
 import useApplicationData from './hooks/useApplicationData';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Link }from "react-router-dom";
 
 const App = () => {
   const {
@@ -9,13 +10,35 @@ const App = () => {
   } = useApplicationData();
     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
 ));
-return (<div className="App" >
-  <h1> Users </h1>
+return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/"}>positronX.io</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/login"}>Login</Link>
+              </li>
+              {/* <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <h1> Users </h1>
+        <ul> {userList} </ul>
+      
+    </div>
+  
+  // <div className="App" >
+//   <h1> Users </h1>
 
-  <ul> {userList} </ul>
-  {/* <Link to="/login">Invoices</Link> |{" "}
-  <Link to="/register">Expenses</Link> */}
-</div >
+//   <ul> {userList} </ul>
+//   {/* <Link to="/login">Invoices</Link> |{" "}
+//   <Link to="/register">Expenses</Link> */}
+// </div >
 );
 };
 
--- client/src/routes/login.js
+++ client/src/routes/login.js
@@ -1,7 +1,17 @@
+import LoginForm from "../components/LoginForm/LoginForm";
+import "../components/LoginForm/LoginForm.css";
+
 export default function Login() {
     return (
+      <div className="login-body">
+      <div className="auth-wrapper">
+        <div className="auth-inner">
       <main style={{ padding: "1rem 0" }}>
         <h2>Login Page</h2>
+        <LoginForm />
       </main>
+      </div>
+      </div>
+      </div>
     );
   }

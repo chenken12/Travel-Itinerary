446798c8774c33a2910118226e465c5ca9cffc05
amnahasad <amnah_asad@hotmail.com>
2022-01-18 17:55:44 -0500

    Work on login authentication

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,11 @@ import useApplicationData from './hooks/useApplicationData';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Link, useNavigate } from "react-router-dom";
 import NavBar from './components/navBar';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+
+  // const [token, setToken] = useState();
 
   const navigate = useNavigate();
 
@@ -19,11 +22,14 @@ const App = () => {
   const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
   ));
 
+  // if (!token) {
+  //   return <LoginForm setToken={setToken} />
+  // }
 
   return (
     <div className="App">
       <NavBar />
-      <ul className="test"> {userList} </ul>
+      {/* <ul className="test"> {userList} </ul> */}
       <button onClick={() => handleRouteClick("/addPins")} type="button">Add Pins page</button>
       <button onClick={() => handleRouteClick("/usersTravels")} type="button">usersTravels page</button>
 

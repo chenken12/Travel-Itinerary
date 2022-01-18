0e6b650fa286f6916db04f79f49e3efa837886f1
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2022-01-18 17:54:37 -0500

    Add control component for form fields in create itinerary

--- client/src/components/CreateItinerary.jsx
+++ client/src/components/CreateItinerary.jsx
@@ -5,24 +5,42 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../styles/CreateItinerary.css';
 export default function CreateItinerary() {
-  const [selectedDate, setDate] = useState(null);
-  const [selectedDate2, setDate2] = useState(null);
+  const state = {
+    name: "",
+    city: "",
+    country: "",
+    startDate: "",
+    endDate: "",
+    details: ""
+
+  }
+  const [formData, setFormData] = useState(state);
+  // const [selectedDate2, setDate2] = useState(null);
   return(
     <Form xs={1}>
       <h1>Create Itinerary</h1>
       <Form.Group className="mb-3">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="name" placeholder="Enter name" />
+        <Form.Control 
+          type="name" 
+          placeholder="Enter name"
+          onChange={name => setFormData({...state, name: name})} />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>City</Form.Label>
-        <Form.Control type="name" placeholder="Enter city" />
+        <Form.Control 
+        type="name" 
+        placeholder="Enter city"
+        onChange={city => setFormData({...state, city: city})} />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>Country</Form.Label>
-        <Form.Select defaultValue="Choose..." placeholder="Enter Country">
+        <Form.Select 
+        defaultValue="Choose..." 
+        placeholder="Enter Country"
+        onChange={country => setFormData({...state, country: country})}>
           <option>US</option>
           <option>Canada</option>
           <option>United Kingdom</option>
@@ -39,16 +57,16 @@ export default function CreateItinerary() {
           <Form.Group as={Col} controlId='formGridStartDate'>
             <Form.Label>Start Date </Form.Label>
             <DatePicker 
-              selected={selectedDate} 
-              onChange={date => setDate(date)}
+              selected={state.startDate} 
+              onChange={date => setFormData({...state, startDate: date})}
               dateFormat='dd/MM/yyyy'
             />
           </Form.Group>
           <Form.Group as={Col} controlId='formGridEndDate'>
             <Form.Label>End Date </Form.Label>
             <DatePicker 
-              selected={selectedDate2} 
-              onChange={date => setDate2(date)}
+              selected={state.endDate} 
+              onChange={date => setFormData({...state, endDate: date})}
               dateFormat='dd/MM/yyyy'
             />
           </Form.Group>
@@ -58,7 +76,11 @@ export default function CreateItinerary() {
         <Form.Label>
           Details
         </Form.Label>
-        <Form.Control type='text' as='textarea' rows={3}>
+        <Form.Control 
+          type='text' 
+          as='textarea' 
+          rows={3} 
+          onChange={text => setFormData({...state, details: text})}>
         </Form.Control>
       </Form.Group>
       <Button variant="flat" type="submit">

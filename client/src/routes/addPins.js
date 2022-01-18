a229454f792c64c3163ab7c2d0cab21c16f62686
Kenneth Chen <kenchen0606@gmail.com>
2022-01-18 00:15:24 -0500

    let addPin fetch from api

--- client/src/routes/addPins.js
+++ client/src/routes/addPins.js
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {displayMarker, displayMarkerInfo} from '../components/DisplayMap'
 import GoogleMapReact from 'google-map-react';
 import "../styles/addPins.css"
 import Marker from '../components/Marker';
+import { useLocation } from 'react-router-dom';
 
 const AddPins = () => {
   const [center, setCenter] = useState({lat: 43.6532, lng: -79.3832 });
   const [zoom, setZoom] = useState(9);
-  let [markerList, setMarkerList] = useState([
+  let [markers, setMarkers] = useState([
     <Marker key={1} lat={43.6532} lng={-79.3832} name="My Marker" color="blue" />,
     <Marker key={2} lat={43.5632} lng={-79.7832} name="My Marker Red" color="red" />
   ]);
+  const [markerInfo, setMarkersInfo] = useState();
+
+  const location = useLocation();
+  const id = location.pathname.split('/')[2];
+
+  useEffect(() => {
+    axios.get(`/api/travels/${id}`)
+      .then((marker) => {
+        setMarkers(() => [displayMarker(marker.data)]);
+        setMarkersInfo(() => [displayMarkerInfo(marker.data)]);
+      });
+  }, []);
 
   const addMark = function(lat, lng) {
-    setMarkerList((prevState) => {
+    setMarkers((prevState) => {
       return [...prevState,
         <Marker lat={lat} lng={lng} name="My Marker" color="blue" cursor='pointer' />
       ];
@@ -33,11 +48,14 @@ const AddPins = () => {
             addMark(event.lat, event.lng);
           }}
         >
-          {markerList}
+          {markers}
           
         </GoogleMapReact>
       </div>
       <button onClick={() => addMark(43.7632, -79.6832)}>Test</button>
+      <div className="MarkerInfo-container">
+        { markerInfo }
+      </div>
     </main>
   );
 }

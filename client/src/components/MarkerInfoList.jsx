424c96f8644797b469bcd7d938ff6a06388711ec
Kenneth Chen <kenchen0606@gmail.com>
2022-01-21 01:17:32 -0500

    marker info page is now sorted

--- client/src/components/MarkerInfoList.jsx
+++ client/src/components/MarkerInfoList.jsx
@@ -1,17 +1,27 @@
 import MarkerInfo from "./MarkerInfo";
-import { getDate } from "../helpers/dateformat";
+import { dateformat } from "../helpers/dateformat";
 
 const MarkerInfoList = (props) => {
   const { day, markerList } = props;
 
-  const parsedInfo = markerList.map((marker, index) => {
+  const markerfilter = markerList.filter((marker) =>{ 
+    // console.log(dateformat(marker.date));
+    return day === dateformat(marker.date);
+    
+  }); 
+
+ 
+  // console.log(markerfilter);
+  const parsedInfo = markerfilter.map((marker, index) => {
     return <MarkerInfo key={`markerinfo${marker.id}`} name={marker.pinned_name} index={ index + 1 }/>;
   });
+  
 
   return (
     <div className='text-box'>
       { day }
-      { parsedInfo }
+      {markerfilter.length > 0 && parsedInfo }
+      {/* {markerfilter.length > 0 && <a>test</a> } */}
     </div>
   );
 };

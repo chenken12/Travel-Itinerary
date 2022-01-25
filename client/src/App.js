1f550861c5016b9576365cda09dde740e55a3578
Kenneth Chen <kenchen0606@gmail.com>
2022-01-25 12:43:54 -0500

    add images to slides

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Slideshow from './components/Slideshow';
 import picture4 from './img4.jpg';
 import picture5 from './img5.jpg';
 import picture6 from './img6.jpg';
+import picture1 from './picture1.jpeg';
+import picture2 from './picture2.jpeg';
+import picture3 from './picture3.jpeg';
 
 const App = () => {
 
@@ -30,7 +33,7 @@ const App = () => {
         <br></br><br></br><br></br><br></br><br></br>
 
         <h2>Create Your Itinerary Today!</h2>
-        <Slideshow />
+        <Slideshow images={[picture1, picture2, picture3]}/>
 
         <button onClick={() => handleRouteClick("/newItinerary")} type="button">Create Now</button>
         <br></br><br></br><br></br><br></br><br></br>

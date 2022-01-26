8b3d443cffb672d35c66aafd726f17113fea3e3d
Kenneth Chen <kenchen0606@gmail.com>
2022-01-26 01:15:46 -0500

    add more buttons

--- client/src/components/UserItinerary.jsx
+++ client/src/components/UserItinerary.jsx
@@ -6,6 +6,14 @@ import DeleteItinerary from "./DeleteItinerary";
 const UserItinerary = (props) => {
   const navigate = useNavigate();
   const {name, description, location, travel_start_date, travel_end_date} = props;
+  console.log(props.id);
+  const editpin = function() {
+    navigate(`/edit/${props.id}`);
+  }
+  const viewpin = function() {
+    navigate(`/view/${props.id}`);
+  }
+
   return (
     <div className="user-travels-page"> 
       <h1>{location}</h1>
@@ -16,8 +24,13 @@ const UserItinerary = (props) => {
         <span>-</span>
         <span>{dateformat(travel_end_date)}</span>
       </div>
-      <EditButton itineraryData={props}/>
-      <DeleteItinerary itineraryData={props}/>
+      <div className="user_button">
+        <EditButton itineraryData={props}/>
+        <DeleteItinerary itineraryData={props}/>
+        <button onClick={() => editpin()}>Edit Pins</button>
+        <button onClick={() => viewpin()}>View Pins</button>
+      </div>
+     
     </div>
   );
 };
